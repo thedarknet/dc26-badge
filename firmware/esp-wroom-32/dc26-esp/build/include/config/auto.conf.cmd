d1313756e83b147c0cec43bff228809b62531349
cmdc0de <cmdc0dez@gmail.com>
2018-01-13 02:35:36 -0700

    updated esp-idf

--- firmware/esp-wroom-32/dc26-esp/build/include/config/auto.conf.cmd
+++ firmware/esp-wroom-32/dc26-esp/build/include/config/auto.conf.cmd
@@ -1,26 +1,26 @@
 deps_config := \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/app_trace/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/aws_iot/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/bt/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/esp32/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/ethernet/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/fatfs/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/freertos/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/heap/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/libsodium/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/log/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/lwip/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/mbedtls/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/openssl/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/pthread/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/spi_flash/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/spiffs/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/tcpip_adapter/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/wear_levelling/Kconfig \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/bootloader/Kconfig.projbuild \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/esptool_py/Kconfig.projbuild \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/partition_table/Kconfig.projbuild \
-	/d/MyStuff/dev/defcon/defcon26/dc26-badge/firmware/esp-wroom-32/esp-idf/Kconfig
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/app_trace/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/aws_iot/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/bt/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/esp32/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/ethernet/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/fatfs/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/freertos/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/heap/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/libsodium/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/log/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/lwip/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/mbedtls/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/openssl/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/pthread/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/spi_flash/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/spiffs/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/tcpip_adapter/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/wear_levelling/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/bootloader/Kconfig.projbuild \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/esptool_py/Kconfig.projbuild \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/partition_table/Kconfig.projbuild \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/Kconfig
 
 include/config/auto.conf: \
 	$(deps_config)

3364ab3237c692e661f4a197bd0379f17c110c66
cmdc0de <cmdc0dez@gmail.com>
2018-04-08 20:49:51 -0700

    compiles start of spi test

--- firmware/esp-wroom-32/dc26-esp/build/include/config/auto.conf.cmd
+++ firmware/esp-wroom-32/dc26-esp/build/include/config/auto.conf.cmd
@@ -2,7 +2,9 @@ deps_config := \
 	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/app_trace/Kconfig \
 	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/aws_iot/Kconfig \
 	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/bt/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/driver/Kconfig \
 	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/esp32/Kconfig \
+	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/esp_adc_cal/Kconfig \
 	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/ethernet/Kconfig \
 	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/fatfs/Kconfig \
 	/home/cmdc0de/dev/darknet/26/dc26-badge/firmware/esp-wroom-32/esp-idf/components/freertos/Kconfig \

9300b6feaace2dbfac0154c551d4b5f09736a333
Gregory Price <gregory@gregoryprice.net>
2018-06-17 20:38:08 -0400

    cleanup

--- firmware/esp-wroom-32/dc26-esp/main/dc26_ble/security.cpp
+++ firmware/esp-wroom-32/dc26-esp/main/dc26_ble/security.cpp
@@ -5,71 +5,33 @@
 #include "../lib/ble/BLESecurity.h"
 #include "../lib/ble/BLEDevice.h"
 
-const char *CLTSECTAG = "ClientSecurityCallbacks";
-const char *SVRSECTAG = "ServerSecurityCallbacks";
+const char *SECTAG = "SecurityCallbacks";
 
 uint32_t MySecurity::onPassKeyRequest()
 {
-	uint32_t passkey = 0;
-	if (pBTTask->isActingClient)
-	{
-		ESP_LOGI(CLTSECTAG, "PassKeyRequest");
-		passkey = 123456;
-		// TODO: randomize
-	}
-	else // is Server
-	{
-		ESP_LOGI(SVRSECTAG, "PassKeyRequest");
-		passkey = 123456;
-		// TODO: request input
-	}
-	return passkey;
+	ESP_LOGI(SECTAG, "PassKeyRequest ********************");
+	// TODO
+	return 123456;
 }
 
 void MySecurity::onPassKeyNotify(uint32_t pass_key)
 {
-	if (pBTTask->isActingClient)
-	{
-		ESP_LOGI(CLTSECTAG, "The passkey Notify number:%d", pass_key);
-	}
-	else
-	{
-		ESP_LOGI(SVRSECTAG, "The passkey Notify number:%d", pass_key);
-	}
+	ESP_LOGI(SECTAG, "The passkey Notify number:%d", pass_key);
 	return;
 }
 
 bool MySecurity::onConfirmPIN(uint32_t pass_key)
 {
-	bool retval = false;
-	if (pBTTask->isActingClient)
-	{
-		ESP_LOGI(CLTSECTAG, "onConfirmPin: %d", pass_key);
-		retval = true;
-	}
-	else
-	{
-		ESP_LOGI(SVRSECTAG, "onConfirmPin %d", pass_key);
-		retval = true;
-		// TODO: retval
-	}
-	return retval;
+	ESP_LOGI(SECTAG, "onConfirmPin: %d", pass_key);
+	// TODO: Send to STM, get back confirmation
+	vTaskDelay(5000 / portTICK_PERIOD_MS);
+	return true;
 }
 
 bool MySecurity::onSecurityRequest()
 {
-	bool retval = false;
-	if (pBTTask->isActingClient)
-	{
-		ESP_LOGI(CLTSECTAG, "On Security Request");
-		retval = true;
-	}
-	else
-	{
-		ESP_LOGI(SVRSECTAG, "On Security Request");
-		retval = true;
-	}
-	return retval;
+	ESP_LOGI(SECTAG, "On Security Request");
+	return true;
 }
 
 void MySecurity::onAuthenticationComplete(esp_ble_auth_cmpl_t auth_cmpl)
@@ -78,20 +40,20 @@ void MySecurity::onAuthenticationComplete(esp_ble_auth_cmpl_t auth_cmpl)
 	{
 		if(auth_cmpl.success)
 		{
-			ESP_LOGI(CLTSECTAG, "remote BD_ADDR:");
-			esp_log_buffer_hex(CLTSECTAG, auth_cmpl.bd_addr, sizeof(auth_cmpl.bd_addr));
-			ESP_LOGI(CLTSECTAG, "address type = %d", auth_cmpl.addr_type);
+			ESP_LOGI(SECTAG, "remote BD_ADDR:");
+			esp_log_buffer_hex(SECTAG, auth_cmpl.bd_addr, sizeof(auth_cmpl.bd_addr));
+			ESP_LOGI(SECTAG, "address type = %d", auth_cmpl.addr_type);
 		}
-		ESP_LOGI(CLTSECTAG, "pair status = %s", auth_cmpl.success ? "success" : "fail");
+		ESP_LOGI(SECTAG, "pair status = %s", auth_cmpl.success ? "success" : "fail");
 	}
 	else
 	{
-		ESP_LOGI(SVRSECTAG, "Starting BLE work!");
+		ESP_LOGI(SECTAG, "Starting BLE work!");
 		if(auth_cmpl.success)
 		{
 			uint16_t length;
 			esp_ble_gap_get_whitelist_size(&length);
-			ESP_LOGI(SVRSECTAG, "size: %d", length);
+			ESP_LOGI(SECTAG, "size: %d", length);
 		}
 	}
 }
